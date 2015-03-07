11bb844d91579556160345eb7ca5799bafad2720
asif chowdhury <asif.chowdhury64@gmail.com>
2015-03-06 20:25:42 -0600

    final commit for png

--- 03 Visualizations/PngCategoricals.R
+++ 03 Visualizations/PngCategoricals.R
@@ -3,11 +3,6 @@ categorical <- function(df, i) {
   ggplot(df, aes(x=i, y=n)) + geom_point() + labs(title = paste("NUMBER of CONTRIBUTORS by", i)) + labs(x = i, y = "Number of Contributors") + theme(plot.title=element_text(size=20, face="bold", vjust=1, family="Bauhaus 93"))
 }
 
-#quantitative <- function(df, i) {
-#  names(df) <- c("i")
-#  ggplot(df, aes(x=i)) + geom_histogram() + labs(title = paste("NUMBER of CONTRIBUTORS by", i)) + labs(x = i, y = "Number of Contributors") + theme(plot.title=element_text(size=20, face="bold", vjust=1, family"Bauhaus 93"))
-#}
-
 categoricals <- eval(parse(text=substring(getURL(URLencode('http://129.152.144.84:5001/rest/native/?query="select * from texas_fed"'), httpheader=c(DB='jdbc:oracle:thin:@129.152.144.84:1521:ORCL', USER='C##cs329e_jc58247', PASS='orcl_jc58247', MODE='native_mode', MODEL='model', returnFor = 'R', returnDimensions = 'True'), verbose = TRUE), 1, 2^31-1)))
 
 l <- list()
@@ -19,21 +14,33 @@ for (i in names(TX_CONT)) {
     l[[i]] <- p
   }
 }
- 
-#m <- list()
-#for (i in names(TX_CONT)) { 
-#  if (i %in% categoricals[[2]]) {
-#    q <- data.frame(fromJSON(getURL(URLencode('129.152.144.84:5001/rest/native/?query="select \\\""i"\\\", count(*) n from texas_fed group by \\\""i"\\\" "'),httpheader=c(DB='jdbc:oracle:thin:@129.152.144.84:1521:ORCL', USER='C##cs329e_jc58247', PASS='orcl_jc58247', MODE='native_mode',MODEL='model',returnDimensions = 'False',returnFor = 'JSON', i=i),verbose = TRUE)))
-#    o <- quantitative(q,i)
-#    print(o) 
-#    m[[i]] <- o
-#  }
-#}
 
-png("categoricals.png", width = 15, height = 10, units = "in", res = 70)
+png("Categoricals.png", width = 15, height = 10, units = "in", res = 70)
 grid.newpage()
 pushViewport(viewport(layout=grid.layout(1,4)))
 print(l[[1]],vp=viewport(layout.pos.row=1,layout.pos.col=1:2))
 print(l[[2]],vp=viewport(layout.pos.row=1,layout.pos.col=3:4))
+
+dev.off()
+
+quantitative <- function(df, x) {
+  names(df) <- c("x")
+  ggplot(df, aes(x=x)) + geom_histogram() + labs(title = paste("NUMBER of CONTRIBUTORS by", i)) + labs(x = i) + theme(plot.title=element_text(size=15, face="bold", vjust=1, family = "Bauhaus 93"))
+}
+l1 <- list()
+for (i in names(TX_CONT)) {   
+  if (i %in% categoricals[[2]]) {
+    r1 <- data.frame(fromJSON(getURL(URLencode('129.152.144.84:5001/rest/native/?query="select \\\""i"\\\", count(*) n from texas_fed group by \\\""i"\\\" "'),httpheader=c(DB='jdbc:oracle:thin:@129.152.144.84:1521:ORCL', USER='C##cs329e_jc58247', PASS='orcl_jc58247', MODE='native_mode',MODEL='model',returnDimensions = 'False',returnFor = 'JSON', i=i),verbose = TRUE)))
+    q <- quantitative(r1,i)
+    print(q) 
+    l1[[i]] <- q
+  }
+}
+png("Quantitative.png", width = 25, height = 10, units = "in", res = 72)
+grid.newpage()
+pushViewport(viewport(layout=grid.layout(1,6)))
+print(l1[[1]],vp=viewport(layout.pos.row=1,layout.pos.col=1:2))
+print(l1[[2]],vp=viewport(layout.pos.row=1,layout.pos.col=3:4))
+print(l1[[3]],vp=viewport(layout.pos.row=1,layout.pos.col=5:6))
 
 dev.off()
