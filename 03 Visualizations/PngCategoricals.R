59e07acd47ed4ba206f283e5d5ef1e6f399cbe0f
asif chowdhury <asif.chowdhury64@gmail.com>
2015-03-06 18:18:35 -0600

    adding png

---
+++ 03 Visualizations/PngCategoricals.R
@@ -0,0 +1,39 @@
+categorical <- function(df, i) {
+  names(df) <- c("i", "n")
+  ggplot(df, aes(x=i, y=n)) + geom_point() + labs(title = paste("NUMBER of CONTRIBUTORS by", i)) + labs(x = i, y = "Number of Contributors") + theme(plot.title=element_text(size=20, face="bold", vjust=1, family="Bauhaus 93"))
+}
+
+#quantitative <- function(df, i) {
+#  names(df) <- c("i")
+#  ggplot(df, aes(x=i)) + geom_histogram() + labs(title = paste("NUMBER of CONTRIBUTORS by", i)) + labs(x = i, y = "Number of Contributors") + theme(plot.title=element_text(size=20, face="bold", vjust=1, family"Bauhaus 93"))
+#}
+
+categoricals <- eval(parse(text=substring(getURL(URLencode('http://129.152.144.84:5001/rest/native/?query="select * from texas_fed"'), httpheader=c(DB='jdbc:oracle:thin:@129.152.144.84:1521:ORCL', USER='C##cs329e_jc58247', PASS='orcl_jc58247', MODE='native_mode', MODEL='model', returnFor = 'R', returnDimensions = 'True'), verbose = TRUE), 1, 2^31-1)))
+
+l <- list()
+for (i in names(TX_CONT)) { 
+  if (i %in% categoricals[[1]]) {
+    r <- data.frame(fromJSON(getURL(URLencode('129.152.144.84:5001/rest/native/?query="select \\\""i"\\\", count(*) n from texas_fed group by \\\""i"\\\" "'),httpheader=c(DB='jdbc:oracle:thin:@129.152.144.84:1521:ORCL', USER='C##cs329e_jc58247', PASS='orcl_jc58247', MODE='native_mode',MODEL='model',returnDimensions = 'False',returnFor = 'JSON', i=i),verbose = TRUE)))
+    p <- categorical(r,i)
+    print(p) 
+    l[[i]] <- p
+  }
+}
+ 
+#m <- list()
+#for (i in names(TX_CONT)) { 
+#  if (i %in% categoricals[[2]]) {
+#    q <- data.frame(fromJSON(getURL(URLencode('129.152.144.84:5001/rest/native/?query="select \\\""i"\\\", count(*) n from texas_fed group by \\\""i"\\\" "'),httpheader=c(DB='jdbc:oracle:thin:@129.152.144.84:1521:ORCL', USER='C##cs329e_jc58247', PASS='orcl_jc58247', MODE='native_mode',MODEL='model',returnDimensions = 'False',returnFor = 'JSON', i=i),verbose = TRUE)))
+#    o <- quantitative(q,i)
+#    print(o) 
+#    m[[i]] <- o
+#  }
+#}
+
+#png("/Users/asif/Documents/GitHub/DataVisualization/DV_RProject3/03 Visualizations/categoricals.png", width = 15, height = 10, units = "in", res = 70)
+#grid.newpage()
+pushViewport(viewport(layout=grid.layout(1,4)))
+print(l[[1]],vp=viewport(layout.pos.row=1,layout.pos.col=1:2))
+print(l[[2]],vp=viewport(layout.pos.row=1,layout.pos.col=3:4))
+
+dev.off()
--- 03 Visualizations/categoricals.R
+++
@@ -1,9 +0,0 @@
-categoricals <- eval(parse(text=substring(getURL(URLencode('http://129.152.144.84:5001/rest/native/?query="select * from texas_fed"'), httpheader=c(DB='jdbc:oracle:thin:@129.152.144.84:1521:ORCL', USER='C##cs329e_jc58247', PASS='orcl_jc58247', MODE='native_mode', MODEL='model', returnFor = 'R', returnDimensions = 'True'), verbose = TRUE), 1, 2^31-1)))
-
-png("categoricals.png", width = 15, height = 10, units = "in", res = 72)
-grid.newpage()
-pushViewport(viewport(layout=grid.layout(2,4)))
-print(categoricals[[1]],vp=viewport(layout.pos.row=1,layout.pos.col=1:2))
-print(categoricals[[2]],vp=viewport(layout.pos.row=1,layout.pos.col=3:4))
-
-dev.off()
